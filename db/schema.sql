3df7aeb1fde3e8697b9f71ee4f5c25d1cdf0f44c
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-03-20 19:48:58 -0700

    added tables in schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE  department (
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (role.id) REFERENCES role (id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (employee.id) REFERENCES employee(id) ON DELETE SET NULL
+);
