d81cb4259d3f37d426d8628303bb22ac13dc485e
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-04-09 17:54:58 -0700

    added functionality

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
@@ -120,10 +111,116 @@ function addDepartment() {
     });
 }
 
-function addRole() {}
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+function addRole() {
+  db.query("SELECT * from department", (err, result) => {
+    if (err) throw err;
 
-function addEmployee() {}
+    const departmentData = result.map(({ id, name }) => ({
+      name: name,
+      value: id,
+    }));
 
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What is the name of the role?",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary of the role?",
+        },
+        {
+          type: "list",
+          name: "department_id",
+          message: "Which department does the role belong to?",
+          choices: departmentData,
+        },
+      ])
+      .then((answer) => {
+        console.log(answer);
+        db.query(
+          `INSERT INTO role (title, salary, department_id) VALUES ("${answer.title}", ${answer.salary}, ${answer.department_id})`,
+          (err, result) => {
+            if (err) throw err;
+            console.log("role added");
+            menu();
+          }
+        );
+      });
+  });
+}
+
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+function addEmployee() {
+  db.query(`SELECT * FROM role`, (err, result) => {
+    if (err) throw err;
+
+    const roleData = result.map(({ id, title }) => ({
+      name: title,
+      value: id,
+    }));
+
+    db.query(`SELECT * FROM employee`, (err, result) => {
+      if (err) throw err;
+
+      const employeeData = result.map(({ first_name, last_name, id }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id,
+      }));
+
+      employeeData.unshift({
+        name: "No manager",
+        value: null,
+      });
+
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "first_name",
+            message: "What is the employee's first name?",
+          },
+          {
+            type: "input",
+            name: "last_name",
+            message: "What is the employee's last name?",
+          },
+          {
+            type: "list",
+            name: "role_id",
+            message: "What is the employee's role?",
+            choices: roleData,
+          },
+          {
+            type: "list",
+            name: "manager_id",
+            message: "Who is this employee's manager?",
+            choices: employeeData,
+          },
+        ])
+        .then((answer) => {
+          console.log(answer);
+          db.query(
+            `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answer.first_name}", "${answer.last_name}", ${answer.role_id}, ${answer.manager_id})`,
+            (err, result) => {
+              if (err) throw err;
+              console.log("role added");
+              menu();
+            }
+          );
+        });
+    });
+  });
+}
+
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 function updateEmployeeRole() {}
 
 menu();
